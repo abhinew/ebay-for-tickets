12f21d3d180ea2a2e95be9983574dd39ad09ee80
abhinew <abhinaya.sv@gmail.com>
2018-10-10 19:27:53 +0200

    logging the react-app-env  for debugging

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,3 +1,3 @@
 export const baseUrl = process.env.REACT_APP_ENV === 'production' ? 'https://tickets-master.herokuapp.com' : 'http://localhost:4000'
-
+console.log("process.env.REACT_APP_ENV", process.env.REACT_APP_ENV);
 export const localStorageJwtKey = 'currentUserJwt'
